f3be7c85b22db9d4f4d35f030c7e196feabe9b8f
Bita Fotovvat <bita.ftvt@gmail.com>
2024-04-01 18:07:50 -0400

    added start script

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 const PORT = process.env.SERVER_PORT || 5050;
 app.get('/', (req, res) => {
-  res.send('Welcome to my API');
+  res.send(`<h1>Welcome to my APP<h1>`);
 });
 
 const requestRoutes = require('./routes/request-routes');
