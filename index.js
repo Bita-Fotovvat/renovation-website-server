0a6b788f2c650d66e49e9dea9fbc127d340594fb
Bita Fotovvat <bita.ftvt@gmail.com>
2024-04-01 18:16:07 -0400

    changed the PORT to be compatible with heroku

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
-const PORT = process.env.SERVER_PORT || 5050;
+const PORT = process.env.PORT || 5050;
 app.get('/', (req, res) => {
   res.send(`<h1>Welcome to my APP<h1>`);
 });
